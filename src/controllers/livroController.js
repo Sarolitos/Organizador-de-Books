d1ba9af8e24530afbc2d4a25edc659298c962ac4
SaraNicoly8 <snsa1@aluno.ifal.edu.br>
2026-06-23 10:57:12 -0300

    Update livroController.js

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,433 +1,122 @@
-import { PrismaClient } from '@prisma/client';
+import livroService from '../services/livroService.js';
 
-const prisma = new PrismaClient();
+class LivroController {
 
-class LivroService {
+    async listar(req, res) {
 
-    validarLivro(livro) {
+    try {
 
-        if (!livro.titulo || livro.titulo.trim() === '') {
-            throw new Error(
-                'O título é obrigatório.'
-            );
+        const busca = req.query.busca || "";
+        let livros;
+        if (busca) {
+            livros = await livroService.buscarLivros(busca);
+        } else {
+            livros = await livroService.listarLivros();
         }
-
-        if (!livro.autor || livro.autor.trim() === '') {
-            throw new Error(
-                'O autor é obrigatório.'
-            );
-        }
-
-        if (!livro.genero || livro.genero.trim() === '') {
-            throw new Error(
-                'O gênero é obrigatório.'
-            );
-        }
-
-        if (!livro.sinopse || livro.sinopse.trim() === '') {
-            throw new Error(
-                'A sinopse é obrigatória.'
-            );
-        }
-
-        if (
-            !livro.anoPublicacao ||
-            livro.anoPublicacao < 1000
-        ) {
-            throw new Error(
-                'Ano de publicação inválido.'
-            );
-        }
-    }
-
-    async listarLivros() {
-
-        return await prisma.livro.findMany({
-            include: {
-                usuario: true
+        
+        res.render(
+            'index',
+            {
+                livros,
+                busca
             }
-        });
+        );
 
-    }
+    } catch (error) {
 
-    async buscarLivros(busca) {
-
-    return await prisma.livro.findMany({
-        where: {
-            OR: [
-                {
-                    titulo: {
-                        contains: busca
-                    }
-                },
-                {
-                    autor: {
-                        contains: busca
-                    }
-                },
-                {
-                    genero: {
-                        contains: busca
-                    }
-                }
-            ]
-        },
-        include: {
-            usuario: true
-        }
-    });
-
-}
-
-    async buscarPorId(id) {
-
-        return await prisma.livro.findUnique({
-            where: {
-                id: Number(id)
-            }
-        });
-
-    }
-
-    async cadastrarLivro(dados) {
-
-        this.validarLivro(dados);
-
-        return await prisma.livro.create({
-            data: {
-                titulo: dados.titulo,
-                autor: dados.autor,
-                genero: dados.genero,
-                anoPublicacao: Number(
-                    dados.anoPublicacao
-                ),
-                sinopse: dados.sinopse,
-                usuarioId: Number(
-                    dados.usuarioId
-                )
-            }
-        });
-
-    }
-
-    async atualizarLivro(id, dados) {
-
-        this.validarLivro(dados);
-
-        return await prisma.livro.update({
-            where: {
-                id: Number(id)
-            },
-            data: {
-                titulo: dados.titulo,
-                autor: dados.autor,
-                genero: dados.genero,
-                anoPublicacao: Number(
-                    dados.anoPublicacao
-                ),
-                sinopse: dados.sinopse
-            }
-        });
-
-    }
-
-    async deletarLivro(id) {
-
-        return await prisma.livro.delete({
-            where: {
-                id: Number(id)
-            }
-        });
+        res.status(500).send(error.message);
 
     }
 }
 
-export default new LivroService();import { PrismaClient } from '@prisma/client';
+    async telaCadastro(req, res) {
 
-const prisma = new PrismaClient();
-
-class LivroService {
-
-    validarLivro(livro) {
-
-        if (!livro.titulo || livro.titulo.trim() === '') {
-            throw new Error(
-                'O título é obrigatório.'
-            );
-        }
-
-        if (!livro.autor || livro.autor.trim() === '') {
-            throw new Error(
-                'O autor é obrigatório.'
-            );
-        }
-
-        if (!livro.genero || livro.genero.trim() === '') {
-            throw new Error(
-                'O gênero é obrigatório.'
-            );
-        }
-
-        if (!livro.sinopse || livro.sinopse.trim() === '') {
-            throw new Error(
-                'A sinopse é obrigatória.'
-            );
-        }
-
-        if (
-            !livro.anoPublicacao ||
-            livro.anoPublicacao < 1000
-        ) {
-            throw new Error(
-                'Ano de publicação inválido.'
-            );
-        }
-    }
-
-    async listarLivros() {
-
-        return await prisma.livro.findMany({
-            include: {
-                usuario: true
-            }
-        });
+        res.render(
+            'cadastrarLivro'
+        );
 
     }
 
-    async buscarLivros(busca) {
+    async cadastrar(req, res) {
 
-    return await prisma.livro.findMany({
-        where: {
-            OR: [
-                {
-                    titulo: {
-                        contains: busca
-                    }
-                },
-                {
-                    autor: {
-                        contains: busca
-                    }
-                },
-                {
-                    genero: {
-                        contains: busca
-                    }
-                }
-            ]
-        },
-        include: {
-            usuario: true
+        try {
+
+            req.body.usuarioId =
+                req.cookies.usuarioId;
+
+            await livroService.cadastrarLivro(
+                req.body
+            );
+
+            res.redirect('/livros');
+
+        } catch (error) {
+
+            res.status(400).send(error.message);
+
         }
-    });
+
+    }
+
+    async telaEditar(req, res) {
+
+        try {
+
+            const livro =
+                await livroService.buscarPorId(
+                    req.params.id
+                );
+
+            res.render(
+                'editarLivro',
+                { livro }
+            );
+
+        } catch (error) {
+
+            res.status(304).send(error.message);
+
+        }
+
+    }
+
+    async atualizar(req, res) {
+
+        try {
+
+            await livroService.atualizarLivro(
+                req.params.id,
+                req.body
+            );
+
+            res.redirect('/livros');
+
+        } catch (error) {
+
+            res.status(400).send(error.message);
+
+        }
+
+    }
+
+    async excluir(req, res) {
+
+        try {
+
+            await livroService.deletarLivro(
+                req.params.id
+            );
+
+            res.redirect('/livros');
+
+        } catch (error) {
+
+            res.status(404).send(error.message);
+
+        }
+
+    }
 
 }
 
-    async buscarPorId(id) {
-
-        return await prisma.livro.findUnique({
-            where: {
-                id: Number(id)
-            }
-        });
-
-    }
-
-    async cadastrarLivro(dados) {
-
-        this.validarLivro(dados);
-
-        return await prisma.livro.create({
-            data: {
-                titulo: dados.titulo,
-                autor: dados.autor,
-                genero: dados.genero,
-                anoPublicacao: Number(
-                    dados.anoPublicacao
-                ),
-                sinopse: dados.sinopse,
-                usuarioId: Number(
-                    dados.usuarioId
-                )
-            }
-        });
-
-    }
-
-    async atualizarLivro(id, dados) {
-
-        this.validarLivro(dados);
-
-        return await prisma.livro.update({
-            where: {
-                id: Number(id)
-            },
-            data: {
-                titulo: dados.titulo,
-                autor: dados.autor,
-                genero: dados.genero,
-                anoPublicacao: Number(
-                    dados.anoPublicacao
-                ),
-                sinopse: dados.sinopse
-            }
-        });
-
-    }
-
-    async deletarLivro(id) {
-
-        return await prisma.livro.delete({
-            where: {
-                id: Number(id)
-            }
-        });
-
-    }
-}
-
-export default new LivroService();import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-class LivroService {
-
-    validarLivro(livro) {
-
-        if (!livro.titulo || livro.titulo.trim() === '') {
-            throw new Error(
-                'O título é obrigatório.'
-            );
-        }
-
-        if (!livro.autor || livro.autor.trim() === '') {
-            throw new Error(
-                'O autor é obrigatório.'
-            );
-        }
-
-        if (!livro.genero || livro.genero.trim() === '') {
-            throw new Error(
-                'O gênero é obrigatório.'
-            );
-        }
-
-        if (!livro.sinopse || livro.sinopse.trim() === '') {
-            throw new Error(
-                'A sinopse é obrigatória.'
-            );
-        }
-
-        if (
-            !livro.anoPublicacao ||
-            livro.anoPublicacao < 1000
-        ) {
-            throw new Error(
-                'Ano de publicação inválido.'
-            );
-        }
-    }
-
-    async listarLivros() {
-
-        return await prisma.livro.findMany({
-            include: {
-                usuario: true
-            }
-        });
-
-    }
-
-    async buscarLivros(busca) {
-
-    return await prisma.livro.findMany({
-        where: {
-            OR: [
-                {
-                    titulo: {
-                        contains: busca
-                    }
-                },
-                {
-                    autor: {
-                        contains: busca
-                    }
-                },
-                {
-                    genero: {
-                        contains: busca
-                    }
-                }
-            ]
-        },
-        include: {
-            usuario: true
-        }
-    });
-
-}
-
-    async buscarPorId(id) {
-
-        return await prisma.livro.findUnique({
-            where: {
-                id: Number(id)
-            }
-        });
-
-    }
-
-    async cadastrarLivro(dados) {
-
-        this.validarLivro(dados);
-
-        return await prisma.livro.create({
-            data: {
-                titulo: dados.titulo,
-                autor: dados.autor,
-                genero: dados.genero,
-                anoPublicacao: Number(
-                    dados.anoPublicacao
-                ),
-                sinopse: dados.sinopse,
-                usuarioId: Number(
-                    dados.usuarioId
-                )
-            }
-        });
-
-    }
-
-    async atualizarLivro(id, dados) {
-
-        this.validarLivro(dados);
-
-        return await prisma.livro.update({
-            where: {
-                id: Number(id)
-            },
-            data: {
-                titulo: dados.titulo,
-                autor: dados.autor,
-                genero: dados.genero,
-                anoPublicacao: Number(
-                    dados.anoPublicacao
-                ),
-                sinopse: dados.sinopse
-            }
-        });
-
-    }
-
-    async deletarLivro(id) {
-
-        return await prisma.livro.delete({
-            where: {
-                id: Number(id)
-            }
-        });
-
-    }
-}
-
-export default new LivroService();
+export default new LivroController();
